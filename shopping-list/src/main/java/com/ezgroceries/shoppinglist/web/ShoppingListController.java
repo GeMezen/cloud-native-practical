c3d62f3bc944d9df5ef96a8fc84229fd2a2f224c
Gert Meeussen <gert.meeussen@kbc.be>
2020-06-02 11:53:49 +0200

    finished the ShoppingListController

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/web/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/web/ShoppingListController.java
@@ -1,6 +1,12 @@
 package com.ezgroceries.shoppinglist.web;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/shopping-lists", produces = "application/json")
 public class ShoppingListController {
+
+
+    @PostMapping
+    public ShoppingListResource post(@RequestBody ShoppingListRequest request) {
+        return new ShoppingListResource(request.getName());
+    }
 
 }
---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/web/ShoppingListResource.java
@@ -0,0 +1,22 @@
+package com.ezgroceries.shoppinglist.web;
+;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import lombok.Builder.Default;
+import lombok.Data;
+
+/**
+ * @author Gert
+ * @since release 202008 / (2020-06-02)
+ */
+@Data
+public class ShoppingListResource {
+    private String shoppingListId ;
+    private String name;
+    
+    public ShoppingListResource(String name) {
+        byte[] bytes = name.getBytes(StandardCharsets.UTF_8);
+        this.shoppingListId = UUID.nameUUIDFromBytes(bytes).toString();
+        this.name = name;
+    }
+}
